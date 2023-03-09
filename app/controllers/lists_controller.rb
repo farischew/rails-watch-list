4143143a19991076c89b9b137e33e4d9dbb295b6
Faris Chew <farischew@gmail.com>
2023-03-09 15:46:30 +0800

    Adds create for list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,9 +11,23 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
