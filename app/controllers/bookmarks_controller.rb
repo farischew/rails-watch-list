e36228ff392dfb01e3e1d512cb2f55ea3ae56b66
Faris Chew <farischew@gmail.com>
2023-03-09 16:13:41 +0800

    Completes create for bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,4 +3,22 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(movie_params)
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
