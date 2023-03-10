4666be00c42c6c062b8c28c23aceb47775df46fb
Faris Chew <farischew@gmail.com>
2023-03-10 16:13:54 +0800

    Bug fixing list show button to new bookmark

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create]
+    resources :bookmarks, only: [:index, :new, :create]
     resources :movies, only: [:index]
   end
 
