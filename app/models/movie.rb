a418e41427ce45ef30023e495af86e44561f68ec
Faris Chew <farischew@gmail.com>
2023-03-10 17:17:08 +0800

    Adds movie poster in list show

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
   has_many :list, through: :bookmark
+  has_many_attached :photo
 
   validates :title, presence: true, uniqueness: true
   validates :title, uniqueness: true
